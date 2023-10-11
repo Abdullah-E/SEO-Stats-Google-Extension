e0e199138a98b93d00b44a82ea9d774dc895f2f6
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2023-10-12 03:42:43 +0500

    initial commit

--- popup.js
+++ popup.js
@@ -9,4 +9,29 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
+const onClickOnButton = () => {
+
+    const toggleButton = document.getElementById('toggleButton');
+    let isOn = false;
+
+    toggleButton.addEventListener('click', function () {
+        if (isOn) {
+            toggleButton.textContent = 'Toggle On';
+            // Add your code to turn off your browser extension functionality here.
+        } else {
+            toggleButton.textContent = 'Toggle Off';
+            // Add your code to turn on your browser extension functionality here.
+        }
+
+        isOn = !isOn;
+    });
+
+};
+
 document.addEventListener("DOMContentLoaded", () => {});
+
+document.addEventListener('DOMContentLoaded', function () {
+    onClickOnButton();
+}
+    
+);
