c7dec1aa42338df8f2c2217f8ec61585733dd263
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2023-11-12 15:28:16 +0500

    Globally enable from popup working

--- background.js
+++ background.js
@@ -1,16 +1,77 @@
-console.log("background.js loaded");
+console.log("background.js loaded")
+
+let extensionState = {
+  global_enable: true,
+  chart_enable: true,
+  word_list_enable: true,
+  stats_enable: true
+}
+chrome.storage.sync.set({'all_states': extensionState}).then(() => {
+  console.log("Saved state: ", extensionState)
+})
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  console.log("Storage changed: ", changes)
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    if(key == "all_states"){
+      console.log("All states changed")
+      extensionState = Object.assign(extensionState, newValue)
+    }
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`) 
+  }
+})
 
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("google.com/search")) {
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
-    const searchWord = urlParameters.get("q");
+  let enableExtension = extensionState.global_enable
+  console.log("Tab updated: ", enableExtension)
+  if (tab.url && tab.url.includes("google.com/search") && enableExtension) {
+    console.log("We in here")
+    const queryParameters = tab.url.split("?")[1]
+    const urlParameters = new URLSearchParams(queryParameters)
+    const searchWord = urlParameters.get("q")
 
-    console.log("Search word: ",searchWord);
+    console.log("Search word: ",searchWord)
 
     chrome.tabs.sendMessage(tabId, {
       type: "SEARCH_WORD",
       searchWord: searchWord,
-    });
+    })
   }
-});
+})
+
+/*
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log("Message received: ", message)
+  switch (message.type) {
+    case "REQ_STATE":
+      console.log("Sending state: ", extensionState)
+      chrome.runtime.sendMessage({
+        type: "RES_STATE",
+        extensionState: extensionState,
+      })
+      break
+    default:
+      break
+  }
+
+    
+  // if(message.type == "EXTENSION_TOGGLE"){
+  //   extensionState = message.extensionState;
+  //   console.log("Extension is now: ", extensionState)
+  // }
+
+})
+
+*/
+/*
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log("Message received: ", message)
+  if(message.type == "EXTENSION_TOGGLE"){
+    enableExtension = message.enableExtension;
+    console.log("Extension is now: ", enableExtension)
+  }
+
+})
+*/
--- popup.js
+++ popup.js
@@ -1,27 +1,54 @@
+const onClickButton = (state) => {
 
-const onClickOnButton = () => {
-
-    const toggleButton = document.getElementById('toggleButton');
-    let isOn = false;
-
+    let isOn = state.global_enable
+    const toggleButton = document.getElementById('toggleButton')
+    console.log("rn: ", isOn)
+    toggleButton.textContent = isOn ? 'Extension is On' : 'Extension is Off'
     toggleButton.addEventListener('click', function () {
-        if (isOn) {
-            toggleButton.textContent = 'Extension is On';
-            // Add your code to turn off your browser extension functionality here.
-        } else {
-            toggleButton.textContent = 'Extension is Off';
-            // Add your code to turn on your browser extension functionality here.
-        }
+        isOn = !isOn
+        state.global_enable = isOn
+        chrome.storage.sync.set({ 'all_states': state })
+        toggleButton.textContent = isOn ? 'Extension is On' : 'Extension is Off'
 
-        isOn = !isOn;
-    });
+        console.log("Extension is now: ", isOn)
+    })
 
-};
-
-document.addEventListener("DOMContentLoaded", () => {});
-
-document.addEventListener('DOMContentLoaded', function () {
-    onClickOnButton();
 }
+
+/*
+const getState = () => {
+    chrome.runtime.sendMessage({
+        type: "REQ_STATE"
+    })
+
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        console.log("Message received: ", message)
+        switch (message.type) {
+            case "RES_STATE":
+                console.log("Receieved state ", message.extensionState)
+                return message.extensionState
+            default:
+                break
+        }
+    })
+
+}
+*/
+// document.addEventListener("DOMContentLoaded", () => {});
+
+const temp_state = {
+    global_enable: true,
+    chart_enable: true,
+    word_list_enable: true,
+    stats_enable: true
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    chrome.storage.sync.get(['all_states'], (result) =>{
+        console.log("Retrieved extension state: ", result)
+        const curr_state = result.all_states ? result.all_states : temp_state
+        onClickButton(curr_state)
+    })
+
     
-);
+})
