034b940b6a36a8883ef77b4276519360303be6d6
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-03-29 05:03:59 +0500

    minor bug lol

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,7 +8,7 @@ import fs from 'fs'
 const oauth_creds = JSON.parse(fs.readFileSync('oauth_creds.json', 'utf8'))
 
 const { client_id, client_secret, redirect_uris } = oauth_creds.web
-const oauth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
+const oauth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[1])
 const scopes = ["https://www.googleapis.com/auth/userinfo.profile"]
 
 fastify.get('/oauth-redirect', async function (req, res) {
@@ -22,46 +22,6 @@ fastify.get('/oauth-redirect', async function (req, res) {
     
 })
 
-fastify.post('/login', async function (req, res) {
-    try{
-        // const { googleId, imageURL, email, name, givenName, familyName } = req.body
-        const user_logged_in = req.body
-        console.log("req.body", user_logged_in)
-        console.log("googleId", user_logged_in.googleId)
-
-        //check if user exists
-        const found_user= await User.findOne({ googleId: user_logged_in.googleId})
-        console.log("found user", found_user)
-        if(!found_user){
-            const new_user = await User.create(user_logged_in)
-            return res.send({ message: 'User created successfully', user: new_user })
-        }
-        else{
-            return res.send({ message: 'User found', user: found_user })
-        }
-        //TODO: hide this
-        /*
-        const jwt_secret = "3247954029"
-        // user found, create a token
-        jwt.sign({
-            email: user.email,
-            id: user._id,
-            name: user.name
-        }, 
-        jwt_secret,
-        {}, 
-        (err,token)=>{
-            if(err) {
-                console.log(err);
-                return res.send({ error: 'Error signing the token' });
-            }
-            res.cookie('token',token).json({ message: 'Logged in successfully', user });
-        })
-        */
-    }catch(error){
-        console.log(error)
-    }
-})
 
 fastify.get('/google-callback', async  (req, res)=> {
     console.log("google callback", req.query)
