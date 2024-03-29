29cc9b3272f17cb950c257ca4669b6eaef06b3ff
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-03-29 06:17:33 +0500

    yay?

--- website_frontend/src/App.js
+++ website_frontend/src/App.js
@@ -5,6 +5,15 @@ import MainPage from './pages/MainPage';
 import DashboardPage from './pages/DashboardPage';
 
 function App() {
+  const Paddle = window.Paddle
+  Paddle.Initialize({ 
+    token: "test_18780c77df0655fc4d02d1b24ec",
+    eventCallback: function(data) {
+      if(data.name == "Checkout.Completed") {
+        console.log(data)
+      }
+    }
+  })
   return (
     <Router>
       <Routes>
