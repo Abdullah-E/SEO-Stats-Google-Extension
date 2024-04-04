5892de57362ea271ed8c954378a76be1bb8549ab
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-04-05 01:03:05 +0500

    mongo models updated

--- server/Models/user.js
+++ server/Models/user.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose"
 const UserSchema = new mongoose.Schema({
     
     id:{type:String, required:true},
-    email:{type:String, required:true},
+    email:{type:String},
     verified_email:{type:Boolean},
     name:{type:String, required:true},
     given_name:{type:String, required:true},
-    family_name:{type:String, required:true},
+    family_name:{type:String},
     picture:{type:String},
     locale:{type:String}
 })
