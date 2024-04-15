8bfae00de965a53efcb673ca2e54f9338080ea2e
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-04-16 04:49:56 +0500

    cookies scope change

--- website_frontend/src/App.js
+++ website_frontend/src/App.js
@@ -12,31 +12,31 @@ function App() {
 
   const [cookies, setCookie, getCookie] = useCookies(["user"])
   console.log("cookies", cookies)
-  const g_id = cookies.user.id
+  // const g_id = cookies.user.id
+
+  const handlePaddleEvent = (data) => {
+    if(data.name == "checkout.completed") {
+
+      console.log(data)
+      const items_arr = data.data.items
+      let total_credits = 0
+      for (let i = 0; i < items_arr.length; i++) {
+        const item = items_arr[i]
+        const credits = item.product.name.split(' ')[0]
+        total_credits += parseInt(credits) * item.quantity
+        
+      }
+      const g_id = cookies.user.id
+      console.log("g_id in paddle callback", g_id)
+      addCredits(g_id, total_credits)
+
+    }
+  }
+
   const Paddle = window.Paddle
   Paddle.Initialize({ 
     token: "test_18780c77df0655fc4d02d1b24ec",
-    eventCallback: function(data) {
-      if(data.name == "checkout.completed") {
-
-        console.log(data)
-        //call add credits here
-        const items_arr = data.data.items
-        let total_credits = 0
-        for (let i = 0; i < items_arr.length; i++) {
-          const item = items_arr[i]
-          const credits = item.product.name.split(' ')[0]
-
-          total_credits += parseInt(credits) * item.quantity
-          
-        }
-        // const g_id = cookies.user.id
-        // console.log(cookies.user)
-        console.log("g_id in paddle callback", g_id)
-        addCredits(g_id, total_credits)
-
-      }
-    }
+    eventCallback: handlePaddleEvent
   })
   return (
     <Router>
