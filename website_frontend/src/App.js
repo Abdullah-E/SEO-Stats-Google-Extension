a7656c7044720e7310b5a7bb0bd24515cd1f1afd
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-04-16 04:22:11 +0500

    payment succes test

--- website_frontend/src/App.js
+++ website_frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {useCookies} from "react-cookie"
 
 import MainPage from './pages/MainPage';
 import DashboardPage from './pages/DashboardPage';
@@ -7,6 +8,8 @@ import DashboardPage from './pages/DashboardPage';
 import { addCredits } from './api/api';
 
 function App() {
+
+  const [cookies, setCookie, getCookie] = useCookies(["user"])
   const Paddle = window.Paddle
   Paddle.Initialize({ 
     token: "test_18780c77df0655fc4d02d1b24ec",
@@ -15,7 +18,19 @@ function App() {
 
         console.log(data)
         //call add credits here
-        
+        const items_arr = data.data.items
+        let total_credits = 0
+        for (let i = 0; i < items_arr.length; i++) {
+          const item = items_arr[i]
+          const credits = item.name.split(' ')[0]
+
+          total_credits += parseInt(credits) * item.quantity
+          
+        }
+        const g_id = cookies.user.id
+        console.log("g_id in paddle callback", g_id)
+        addCredits(g_id, total_credits)
+
       }
     }
   })
