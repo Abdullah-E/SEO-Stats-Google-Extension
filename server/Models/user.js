d052287d6a734f61de5576d8b138721843f69e7a
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-04-15 05:52:40 +0500

    credits to user model

--- server/Models/user.js
+++ server/Models/user.js
@@ -9,8 +9,16 @@ const UserSchema = new mongoose.Schema({
     given_name:{type:String, required:true},
     family_name:{type:String},
     picture:{type:String},
-    locale:{type:String}
+    locale:{type:String},
+    credits:{type:Number,required:true,default:0},
 })
 
+UserSchema.pre('save', function(next) {
+   
+    if (this.isNew) {
+        this.credits = 0;
+    }
+    next();
+});
 
 export const User = mongoose.model('User', UserSchema)
