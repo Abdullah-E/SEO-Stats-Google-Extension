392eacd8e9feb7cc9e322dda654015ee6bc611b6
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-04-15 05:56:56 +0500

    subtract credits

--- server/routes/keywordRoutes.js
+++ server/routes/keywordRoutes.js
@@ -7,7 +7,18 @@ fastify.get('/google_search', async function (req, res) {
         console.log("keyword", keyword)
         console.log("g_id", g_id)
 
-        res.send({ message: 'Success' })
+        // res.send({ message: 'Success' })
+
+        const findUser = await User.findOne({ id: g_id })
+        if (findUser == null) {
+            return res.send({ error: 'User not found' })
+        }
+        console.log("findUser", findUser)
+        findUser.credits -= 1
+        await findUser.save()
+
+        return res.send({ message: 'Success', user: findUser })
+
         
     } catch (error) {
         console.log(error)
