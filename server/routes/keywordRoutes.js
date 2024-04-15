a9f73c801960e891c9b2de6b676e983bd6f3dd17
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-04-15 05:43:09 +0500

    debugging

--- server/routes/keywordRoutes.js
+++ server/routes/keywordRoutes.js
@@ -3,8 +3,11 @@ import { fastify } from './init.js'
 fastify.get('/google_search', async function (req, res) {
     try {
         const { keyword, g_id} = req.query
+        console.log("whole query", req.query)
         console.log("keyword", keyword)
         console.log("g_id", g_id)
+
+        res.send({ message: 'Success' })
         
     } catch (error) {
         console.log(error)
